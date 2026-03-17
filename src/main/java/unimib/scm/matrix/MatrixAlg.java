7c2654d185750394894bf252b6ab2ed575daf034
spitfire233 <87020972+spitfire233@users.noreply.github.com>
2026-03-17 23:24:19 +0100

    Implemented utility methods to check if a matrix is lower/upper triangular

--- src/main/java/unimib/scm/matrix/MatrixAlg.java
+++ src/main/java/unimib/scm/matrix/MatrixAlg.java
@@ -32,40 +32,18 @@ public class MatrixAlg {
         return isLowerTriangular;
     }
 
-    // TODO: Refactorare in modo tale che funzioni con isMatrixLowerTriangular ma con la trasposta!
     public static boolean isMatrixUpperTriangular(double[][] matrix) throws IllegalArgumentException {
-        if(!isMatrixSquared(matrix))
-            throw new IllegalArgumentException(MATRIX_NOT_SQUARED_ERROR_MSG);
-        boolean isUpperTriangular = true;
-        int i = matrix.length - 1;
-        while (i >= 0 && isUpperTriangular) {
-            int j = i - 1;
-            while (j >= 0) {
-                if(matrix[i][j] != 0.0) {
-                    isUpperTriangular = false;
-                    break;
-                }
-                j--;
+        return isMatrixLowerTriangular(transposeMatrix(matrix));
+    }
+
+    public static double[][] transposeMatrix(double[][] matrix) {
+        double[][] transpose = new double[matrix[0].length][];
+        for (int i = 0; i < matrix[0].length; i++) {
+            transpose[i] = new double[matrix.length];
+            for(int j = 0; j < matrix.length; j++) {
+                transpose[i][j] = matrix[j][i];
             }
-            i--;
         }
-        return isUpperTriangular;
+        return transpose;
     }
-
-
-
-
-
-    public static int[] solveTriangularSystem(double[][] matrix) throws IllegalArgumentException {
-        int[] x = new int[matrix.length];
-
-
-
-        return x;
-    }
-
-
-
-
-
 }
--- src/test/java/unimib/scm/matrix/MatrixAlgUnitTest.java
+++ src/test/java/unimib/scm/matrix/MatrixAlgUnitTest.java
@@ -1,53 +1,94 @@
 package unimib.scm.matrix;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
 import java.util.Random;
+import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MatrixAlgUnitTest {
 
-    @Test
-    public void whenMatrixSquared_thenReturnTrue() {
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3,4})
+    public void whenMatrixSquared_thenReturnTrue(int n) {
         Random random = new Random();
-        int n = random.nextInt(5) + 1;
         double[][] matrix = new double[n][n];
-        assertTrue(MatgrixAlg.isMatrixSquared(matrix));
+        assertTrue(MatrixAlg.isMatrixSquared(matrix));
     }
 
-    @Test
-    public void whenMatrixNotSquared_thenReturnFalse() {
-        Random random = new Random();
-        int n = random.nextInt(5) + 1;
-        int m = random.nextInt(5) + 1;
+    @ParameterizedTest
+    @CsvSource({
+            "1, 2",
+            "2, 1",
+            "3, 4",
+            "5, 3"
+    })
+    public void whenMatrixNotSquared_thenReturnFalse(int n, int m) {
         double[][] matrix = new double[n][m];
         assertFalse(MatrixAlg.isMatrixSquared(matrix));
     }
 
-    public void whenMatrixLowerTriangular_thenReturnTrue() {
 
-    }
-
-
-
-    private double[][] generateRandomLowerTriangularMatrix() {
+    @ParameterizedTest
+    @CsvSource({
+            "2, 2",
+            "3, 4",
+            "4, 3",
+            "5, 3",
+            "6, 1"
+    })
+    public void whenTransposingMatrix_returnCorrectMatrix(int n, int m) {
         Random random = new Random();
-        int n = random.nextInt(5) +1;
-        double[][] matrix = new double[n][n];
+        double[][] matrix = new double[n][m];
+        double[][] correctTranspose = new double[m][n];
 
-        for(int i = 0; i < matrix.length; i++) {
-            for(int j = i + 1; j < matrix[i].length; j++) {
-                matrix[i][j] = random.nextDouble();
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                double entry = random.nextDouble();
+                matrix[i][j] = entry;
+                correctTranspose[j][i] = entry;
             }
         }
-
-        return matrix;
-
-
+        double[][] result = MatrixAlg.transposeMatrix(matrix);
+        assertTrue(IntStream.range(0, m)
+                .allMatch(i -> Arrays.equals(correctTranspose[i], result[i])));
     }
 
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4})
+    public void whenMatrixIsLowerTriangular_returnTrue(int n) {
+        Random random = new Random();
+        double[][] matrix = new double[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j <= i) {
+                    matrix[i][j] = random.nextDouble();
+                } else {
+                    matrix[i][j] = 0.0;
+                }
+            }
+        }
+        assertTrue(MatrixAlg.isMatrixLowerTriangular(matrix));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4})
+    public void whenMatrixIsUpperTriangular_returnTrue(int n) {
+        Random random = new Random();
+        double[][] matrix = new double[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j >= i) {
+                    matrix[i][j] = random.nextDouble();
+                } else {
+                    matrix[i][j] = 0.0;
+                }
+            }
+        }
+        assertTrue(MatrixAlg.isMatrixUpperTriangular(matrix));
+    }
 }
